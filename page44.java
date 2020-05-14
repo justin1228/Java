b57b33479cb346743f91273a927cfd717998841a
justin1228 <didls1228@gmail.com>
2020-05-14 19:16:38 +0900

    page44

---
+++ page44.java
@@ -0,0 +1,11 @@
+public class page44{
+    public static void main (String[] args){
+        int x = 3 ;
+        String name = "Dirk";
+        x = x*17;
+        System.out.println("x is "+x+" !");
+        double rand = Math.floor(Math.random()*100);
+
+        System.out.println(rand);
+    }
+}
